ab3f1d26c6864886696e127b4a23fe5a68147d10
Damir <37576747+DamirAkzhigitov@users.noreply.github.com>
2025-06-01 23:49:46 +0300

    feat: Test coverage
    * feat: Change README.md
    
    * fix: replace matrix to single node version
    
    * fix: remove - from uses
    
    * fix types
    
    * fix prettier, name of commands
    
    * ci.yml update
    
    * coverage level

--- apps/web/__test__/components/Hero.test.tsx
+++ apps/web/__test__/components/Hero.test.tsx
@@ -1,12 +1,12 @@
-import { render, screen } from '@testing-library/react'
-import Hero from '../../app/components/Hero'
-import '@testing-library/jest-dom'
+import { render, screen } from "@testing-library/react";
+import Hero from "../../app/components/Hero";
+import "@testing-library/jest-dom";
 
-describe('Hero Component', () => {
-    it('renders hero heading', () => {
-        render(<Hero />)
-        const heading = screen.getByRole('heading', { level: 1 })
-        expect(heading).toBeInTheDocument()
-        expect(heading).toHaveTextContent("Damir Akzhigitov")
-    })
-})
+describe("Hero Component", () => {
+  it("renders hero heading", () => {
+    render(<Hero />);
+    const heading = screen.getByRole("heading", { level: 1 });
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent("Damir Akzhigitov");
+  });
+});
