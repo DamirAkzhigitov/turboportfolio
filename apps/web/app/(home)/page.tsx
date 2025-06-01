2c14d85382c7f890f4f2f5d8de3b92ea6b401747
Damir Akzhigitov <damir.cy@gmail.com>
2025-06-01 18:25:50 +0300

    feat: Setup jest testing and GitHub actions

---
+++ apps/web/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import classes from "./page.module.css";
+
+export default function HomePage() {
+    return (
+        <div className={classes.page}>
+
+        </div>
+    )
+}
---
+++ apps/web/jest.setup.ts
@@ -0,0 +1,34 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+
+// Mock next/router
+jest.mock('next/router', () => ({
+    useRouter() {
+        return {
+            route: '/',
+            pathname: '/',
+            query: '',
+            asPath: '',
+            push: jest.fn(),
+            pop: jest.fn(),
+            reload: jest.fn(),
+            back: jest.fn(),
+            prefetch: jest.fn().mockResolvedValue(undefined),
+            beforePopState: jest.fn(),
+            events: {
+                on: jest.fn(),
+                off: jest.fn(),
+                emit: jest.fn(),
+            },
+        }
+    },
+}))
+
+// Mock next/image
+jest.mock('next/image', () => ({
+    __esModule: true,
+    default: (props: any) => {
+        // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
+        return React.createElement('img', props)
+    },
+}))
