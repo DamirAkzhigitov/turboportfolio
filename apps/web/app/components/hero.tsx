2d16dd4d3cdacebf045216de37b0e475bb025c9a
Damir Akzhigitov <damir.cy@gmail.com>
2025-06-04 22:33:17 +0300

    chore: add test to pass coverage

--- apps/web/app/components/hero.tsx
+++ apps/web/app/components/hero.tsx
@@ -2,7 +2,7 @@ import classes from './hero.module.css';
 
 export default function Hero() {
   return (
-    <section className={classes.container}>
+    <section className={classes.container} data-testid="hero-container">
       <div className={classes.hero}>
         <h1 className={classes.title}>Damir Akzhigitov</h1>
         <h3>Senior Frontend Engineer</h3>
