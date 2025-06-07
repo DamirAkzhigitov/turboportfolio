ea8390baef95f01a1978083b21ed9ff71b200650
Damir Akzhigitov <damir.cy@gmail.com>
2025-06-07 17:05:21 +0300

    mock intersection observer

--- apps/web/__test__/page.test.tsx
+++ apps/web/__test__/page.test.tsx
@@ -2,7 +2,21 @@ import { render, screen } from '@testing-library/react';
 import Page from '../app/page';
 import '@testing-library/jest-dom';
 
+import { MotionGlobalConfig } from 'motion';
+
+MotionGlobalConfig.skipAnimations = true;
+
 describe('Home page', () => {
+  beforeEach(() => {
+    const mockIntersectionObserver = jest.fn();
+    mockIntersectionObserver.mockReturnValue({
+      observe: () => null,
+      unobserve: () => null,
+      disconnect: () => null,
+    });
+    window.IntersectionObserver = mockIntersectionObserver;
+  });
+
   it('renders container', () => {
     render(<Page />);
 
