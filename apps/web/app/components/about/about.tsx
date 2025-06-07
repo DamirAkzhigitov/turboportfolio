72fbf30125b514445bdbade40df8db1233166c73
Damir <37576747+DamirAkzhigitov@users.noreply.github.com>
2025-06-07 17:08:41 +0300

    feat: About me secrtion (#4)
    * about me part
    
    * feat: about page
    
    * fixes for mobile version
    
    * add small animations
    
    * mock intersection observer

---
+++ apps/web/app/components/about/about.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import IReact from '../icons/react';
+import ITs from '../icons/typescript';
+import IVue from '../icons/vue';
+import classes from './about.module.css';
+
+import * as motion from 'motion/react-client';
+
+const transition = (delay: number) => ({
+  duration: 2,
+  delay,
+});
+
+export default function About() {
+  return (
+    <section className={classes.container}>
+      <motion.h3
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={transition(0)}
+        className={classes.subtitle}
+      >
+        About me
+      </motion.h3>
+      <motion.h2
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={transition(0.5)}
+        className={classes.title}
+      >
+        Hi there!
+      </motion.h2>
+      <motion.p
+        className={classes.description}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={transition(1)}
+      >
+        I’m <strong>Frontend developer</strong> with a passion for user
+        experience and clean, scalable code. Since 2018, I’ve been building
+        responsive, accessible web applications using modern frameworks like{' '}
+        <IReact className={classes.icon} />
+        <strong>React</strong> and <IVue className={classes.icon} />
+        <strong className={classes.vue}>Vue.js</strong>, with a strong focus on
+        performance, usability, and maintainability. I write robust, type-safe
+        code with <ITs className={classes.icon} />
+        <strong>TypeScript</strong> and stay current with the latest frontend
+        technologies and best practices.
+      </motion.p>
+      <p>Want to know more about me?</p>
+      <div>
+        <a
+          className={classes.connect}
+          href="https://www.linkedin.com/in/damir-akzhigitov/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Let's Connect
+        </a>
+      </div>
+    </section>
+  );
+}
---
+++ apps/web/app/components/icons/react.tsx
@@ -0,0 +1,33 @@
+import type { CSSProperties } from 'react';
+
+export default function IReact({
+  style,
+  className,
+  width = 24,
+  height = 24,
+}: {
+  className?: string;
+  style?: CSSProperties;
+  width?: number;
+  height?: number;
+}) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      className={className}
+      style={style}
+      viewBox="-10.5 -9.45 21 18.9"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>React logo by Meta, licensed under CC BY 4.0</title>
+      <circle cx="0" cy="0" r="2" fill="rgb(88, 196, 220)" />
+      <g stroke="rgb(88, 196, 220)" strokeWidth="1" fill="none">
+        <ellipse rx="10" ry="4.5" />
+        <ellipse rx="10" ry="4.5" transform="rotate(60)" />
+        <ellipse rx="10" ry="4.5" transform="rotate(120)" />
+      </g>
+    </svg>
+  );
+}
