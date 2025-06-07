f3be0f41fb74caddb75bd7c8609d1f4fe9d7fde2
Damir Akzhigitov <damir.cy@gmail.com>
2025-06-07 16:25:11 +0300

    add small animations

--- apps/web/app/components/about/about.tsx
+++ apps/web/app/components/about/about.tsx
@@ -1,14 +1,42 @@
+'use client';
+
 import IReact from '../icons/react';
 import ITs from '../icons/typescript';
 import IVue from '../icons/vue';
 import classes from './about.module.css';
 
+import * as motion from 'motion/react-client';
+
+const transition = (delay: number) => ({
+  duration: 2,
+  delay,
+});
+
 export default function About() {
   return (
     <section className={classes.container}>
-      <h3 className={classes.subtitle}>About me</h3>
-      <h2 className={classes.title}>Hi there!</h2>
-      <p className={classes.description}>
+      <motion.h3
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={transition(0)}
+        className={classes.subtitle}
+      >
+        About me
+      </motion.h3>
+      <motion.h2
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={transition(0.5)}
+        className={classes.title}
+      >
+        Hi there!
+      </motion.h2>
+      <motion.p
+        className={classes.description}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={transition(1)}
+      >
         I’m <strong>Frontend developer</strong> with a passion for user
         experience and clean, scalable code. Since 2018, I’ve been building
         responsive, accessible web applications using modern frameworks like{' '}
@@ -19,7 +47,7 @@ export default function About() {
         code with <ITs className={classes.icon} />
         <strong>TypeScript</strong> and stay current with the latest frontend
         technologies and best practices.
-      </p>
+      </motion.p>
       <p>Want to know more about me?</p>
       <div>
         <a
